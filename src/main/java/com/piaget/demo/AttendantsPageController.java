0a4aa5e010652cd8a33c78de8d6a4d572a3fd7e0
João Seixas <joao.seixas@outlook.pt>
2020-05-19 19:47:00 +0100

    Modificação da pagina de attendants
    Criação de uma tabela dinâmica que é preenchida com a adição de attendants

--- src/main/java/com/piaget/demo/AttendantsPageController.java
+++ src/main/java/com/piaget/demo/AttendantsPageController.java
@@ -1,12 +1,50 @@
 package com.piaget.demo;
 
+import com.piaget.demo.entities.Attendant;
+import com.piaget.demo.repositories.AttendantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class AttendantsPageController {
+    private AttendantRepository attendantRepository;
+
+    @Autowired
+    public AttendantsPageController(AttendantRepository repository) {
+        this.attendantRepository = repository;
+    }
+
+
+    @GetMapping("/createAttendants")
+    public String createAttendants(Attendant attendant) {
+        return "add-attendant";
+    }
+
+
+    @PostMapping("/addAttendant")
+    public String addAttendant(@Valid Attendant attendant, BindingResult result,  Model model) {
+        attendantRepository.save(attendant);
+        model.addAttribute("attendants", attendantRepository.findAll());
+        return "attendantspage";
+    }
+
+
+
+
+
+
+
+
+
+
 
     @GetMapping("/attendantspage")
     public String attendants(Model model)
