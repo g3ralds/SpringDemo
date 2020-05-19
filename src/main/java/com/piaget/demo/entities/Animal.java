f0a7d729ca13a1c196c32d9ab76ed92c53975e8a
João Seixas <joao.seixas@outlook.pt>
2020-05-19 15:29:59 +0100

    Add database classes and test

---
+++ src/main/java/com/piaget/demo/entities/Animal.java
@@ -0,0 +1,41 @@
+package com.piaget.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String species;
+
+    protected Animal() {}
+
+    public Animal(String name, String species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Animal[id=%d, name='%s', species='%s']",
+                id, name, species);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+}
---
+++ src/main/java/com/piaget/demo/repositories/AnimalRepository.java
@@ -0,0 +1,11 @@
+package com.piaget.demo.repositories;
+
+import com.piaget.demo.entities.Animal;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AnimalRepository extends CrudRepository<Animal, Long> {
+    List<Animal> findBySpecies(String species);
+    Animal findById(long id);
+}
