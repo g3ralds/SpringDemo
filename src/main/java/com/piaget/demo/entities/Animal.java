14ebc4d8d79ef02d243b98819469a1598e2f755c
Pedro Geraldes <>
2020-05-26 21:27:37 +0100

    Add changes to Animal

--- src/main/java/com/piaget/demo/entities/Animal.java
+++ src/main/java/com/piaget/demo/entities/Animal.java
@@ -1,5 +1,7 @@
 package com.piaget.demo.entities;
 
+import com.piaget.demo.Satisfaction;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +16,27 @@ public class Animal {
 
     private String name;
     private String species;
+    private int age;
+    private String avatar;
+    private int satisfaction;
+
+    public int getSatisfaction() {
+        return satisfaction;
+    }
+
+    public void setSatisfaction(int satisfaction) {
+        this.satisfaction = satisfaction;
+    }
+
+    public Habitat getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(Habitat habitat) {
+        this.habitat = habitat;
+    }
+
+    private Habitat habitat;
 
     protected Animal() {}
 
@@ -22,24 +45,56 @@ public class Animal {
         this.species = species;
     }
 
-    @Override
-    public String toString() {
-        return String.format(
-                "Animal[id=%d, name='%s', species='%s']",
-                id, name, species);
-    }
-
     public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getSpecies() {
         return species;
     }
 
-    public void setId(long id) { this.id = id; }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Animal[id=%d, name='%s', species='%s']", id, name, species);
+    }
+
+
+    public void calculateSatisfaction() {
+        satisfaction = Satisfaction.calculate(this);
+    }
+
+    public boolean EqualsTo(Animal a) {
+    }
 }
