055ae1260eafa4805cc305783aabda6a6faaee74
João Seixas <joao.seixas@outlook.pt>
2020-06-02 21:07:38 +0100

    Update à página de Habitats

--- src/main/java/com/piaget/demo/entities/Habitat.java
+++ src/main/java/com/piaget/demo/entities/Habitat.java
@@ -11,22 +11,42 @@ public class Habitat {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
 
+    private String name;
+    private String avatar;
+    private int area;
+
     @OneToMany
     private List<Animal> animals = new ArrayList<Animal>();
 
-    private String name;
-    private int area;
-
-    public String getName() {
-        return name;
-    }
-
-    public Habitat() {
-    }
+    public Habitat() { }
 
     public Habitat(String name, int area) {
         this.name = name;
         this.area = area;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void addAnimal(Animal animal) {
+        this.animals.add(animal);
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
@@ -41,16 +61,11 @@ public class Habitat {
         this.area = area;
     }
 
-    public List<Animal> getAnimals() {
-        return animals;
-    }
-
-    public void addAnimals(Animal animal) {
-        this.animals.add(animal);
+    public long getId() {
+        return id;
     }
 
     public void setId(long id) {
         this.id = id;
     }
-
 }
